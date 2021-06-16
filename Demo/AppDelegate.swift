3f74b189e948d558b95ded1220b2cccda086b16e
Viet Nguyen Tran <vietstone.ng@gmail.com>
2021-06-16 09:57:51 +0700

    first commit

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RocketCode
+//
+//  Created by Viet Nguyen Tran on 2021-06-16.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ RocketCode/Test.swift
@@ -0,0 +1,12 @@
+//
+//  Test.swift
+//  RocketCode
+//
+//  Created by Viet Nguyen Tran on 2021-06-16.
+//
+
+import Foundation
+
+class Test {
+    
+}
